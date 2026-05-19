66d1b3ef8c14a65cd6f112d3ddf510c9ee8ad72b
Tessa Demel <254993102+tedemel@users.noreply.github.com>
2026-05-19 21:13:58 +0200

    chore: promote to STABLE and bump release to 1.0.0
    - Promote $plugin->maturity from MATURITY_ALPHA to MATURITY_STABLE
    - Bump version 2026050300 -> 2026051902
    - Bump release 0.1.0 -> 1.0.0

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_courseshift';
-$plugin->version   = 2026050300;
-$plugin->requires  = 2025041400;
+$plugin->version   = 2026051902;
+$plugin->requires  = 2025041400; // Moodle 5.0.
 $plugin->supported = [500, 502];
-$plugin->maturity  = MATURITY_ALPHA;
-$plugin->release   = '0.1.0';
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = '1.0.0';
 $plugin->dependencies = [];
